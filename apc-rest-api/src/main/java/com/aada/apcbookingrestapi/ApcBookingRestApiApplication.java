75a95db848003093b13300ff8ec31611bed3f0a8
dinhvandong <dongvan.ict3@gmail.com>
2024-03-02 23:30:50 +0700

    first setup
    update

---
+++ apc-rest-api/src/main/java/com/aada/apcbookingrestapi/ApcBookingRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.aada.apcbookingrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApcBookingRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApcBookingRestApiApplication.class, args);
+	}
+
+}
---
+++ apc-rest-api/src/test/java/com/aada/apcbookingrestapi/ApcBookingRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aada.apcbookingrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApcBookingRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
